144b6860dec4e903c5a05757f7688e426e308cef
DeveloperHB <167189840+DeveloperHB@users.noreply.github.com>
2024-04-28 22:45:09 +0900

    Add files via upload

---
+++ ch14/sec06/exam01/Calculator.java
@@ -0,0 +1,29 @@
+package ch14.sec06.exam01;
+
+public class Calculator {
+	private int memory;
+	
+	public int getMemory() {
+		return memory;
+		
+	}
+	
+	public synchronized void setMemory1(int memory) {
+		this.memory = memory;   //메모리값 저장
+		try {
+		 Thread.sleep(2000); //2초간 일시정지
+		} catch(InterruptedException e)  {}
+		System.out.println(Thread.currentThread().getName() + ":" + this.memory);
+	}
+	
+	public void setMemory2(int memory) {
+		synchronized(this) {
+			this.memory = memory;
+			try {
+				Thread.sleep(2000);
+			} catch(InterruptedException e) {}
+			System.out.println(Thread.currentThread().getName() + ": " + this.memory);
+		}
+	}
+
+}
