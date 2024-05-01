a9fa66461aff423e65aec34e2343cd596787859f
DeveloperHB <167189840+DeveloperHB@users.noreply.github.com>
2024-05-01 23:53:48 +0900

    Add files via upload

---
+++ practice/chap03/sec12/Exam02.java
@@ -0,0 +1,11 @@
+package practice.chap03.sec12;
+
+public class Exam02 {
+
+	public static void main(String[] args) {
+		int score = 155;
+		String result = (!(score>90))? "가":"나";
+		System.out.println(result);
+	}
+
+}
