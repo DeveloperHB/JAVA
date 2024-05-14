df1565ac36563319e996af1477b66a35102b1fdd
DeveloperHB <167189840+DeveloperHB@users.noreply.github.com>
2024-05-14 18:21:15 +0900

    Add files via upload

--- Test3/shoppingmall.js
+++ Test3/shoppingmall.js
@@ -2,7 +2,7 @@
 const productsContainer = document.getElementById('products');
 
 const products = [
-    { name: 'T-Shirt', price: 19.99, images: 'tshirt.jpg' },
+    { name: 'T-Shirt', price: 19.99, image: 'tshirt.jpg' },
     { name: 'Jeans', price: 39.99, image: 'jeans.jpg' },
     { name: 'Dress', price: 49.99, image: 'dress.jpg' }
 ];
