d66ed68ecf0605d59d89e8960d9f618304a5bf8d
DeveloperHB <167189840+DeveloperHB@users.noreply.github.com>
2024-05-15 20:42:48 +0900

    Add files via upload

---
+++ p0515/CastingExample.java
@@ -0,0 +1,24 @@
+package p0515;
+
+public class CastingExample {
+
+	public static void main(String[] args) {
+		int var1 = 80;
+		byte var2 = (byte) var1;
+		System.out.println(var2);
+		
+		long var3 = 800;
+		int var4 = (int) var3;
+		System.out.println(var4);
+		
+		int var5 = 1088;
+		char var6 = (char)var5;
+		System.out.println(var6);
+		
+		double var7 =8.125166;
+		int var8 = (int)var7;
+		System.out.println(var8);
+
+	}
+
+}
