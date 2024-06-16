d0043626070b3d7ce95846e2323bcbe5789299e4
DeveloperHB <167189840+DeveloperHB@users.noreply.github.com>
2024-06-16 23:52:49 +0900

    Add files via upload

---
+++ p0616/Exam02.java
@@ -0,0 +1,12 @@
+package p0616;
+
+public class Exam02 {
+
+	public static void main(String[] args) {
+		int score = 95;
+		String result = (!(score>90))? "가":"나";
+		System.out.println(result);
+
+	}
+
+}
---
+++ p0616/Exam03.java
@@ -0,0 +1,17 @@
+package p0616;
+
+public class Exam03 {
+
+	public static void main(String[] args) {
+		int pencils = 534;
+		int students = 30;
+		
+		int pencilPerStudent = (pencils/students);
+		System.out.println(pencilPerStudent);
+		
+		int pencilLeft = (20);
+		System.out.println(pencilLeft);
+
+	}
+
+}
