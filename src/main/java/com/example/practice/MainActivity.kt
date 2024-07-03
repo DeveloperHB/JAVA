03849ee15f8a9a0cda8e65d480933cc68fad37ca
DeveloperHB <167189840+DeveloperHB@users.noreply.github.com>
2024-07-03 18:25:20 +0900

    Add files via upload

--- src/main/java/com/example/practice/MainActivity.kt
+++ src/main/java/com/example/practice/MainActivity.kt
@@ -3,44 +3,79 @@ package com.example.practice
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import java.util.Random
 import java.util.Timer
 import kotlin.concurrent.timer
+import kotlin.math.abs
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+    var p_num = 3
+    var k = 1
+    val point_List = mutableListOf<Float>()
 
+
+    fun main() {
         setContentView(R.layout.activity_main)
-
         var timerTask: Timer? = null
-        var isRunning = false
+        var stage = 1
         var sec: Int = 0
-        val tv: TextView = findViewById(R.id.tv_hello)
+        val tv_point: TextView = findViewById(R.id.tv_point)
+        val tv_timer: TextView = findViewById(R.id.tv_timer)
+        val tv_random: TextView = findViewById(R.id.tv_random)
+        val tv_people: TextView = findViewById(R.id.tv_people)
         val btn: Button = findViewById(R.id.btn_kor)
+        val random_box = Random()
+        val num = random_box.nextInt(1001)
+
+        tv_random.text = ((num.toFloat()) / 100).toString()
+        btn.text = "시작"
+        tv_people.text = "참가자 $k"
 
         btn.setOnClickListener {
-            isRunning = !isRunning
-            if (isRunning) {
-                timerTask = timer(period = 1000) {
+            stage++
+            if (stage == 2) {
+                timerTask = timer(period = 10) {
+                    sec++
                     runOnUiThread {
-                        sec++
-                        tv.text = sec.toString()
+                        tv_timer.text = (sec.toFloat() / 100).toString()
                     }
 
                 }
-            } else {
+                btn.text = "정지"
+            } else if (stage == 3) {
                 timerTask?.cancel()
-
-
+                val point = abs(sec - num).toFloat() / 100
+                point_List.add(point)
+                tv_point.text = point.toString()
+                btn.text = "다음"
+                stage = 0
+            } else if (stage == 1) {
+                if (k < p_num) {
+                    k++
+                    main()
+                } else {
+                    btn.text = "게임종료"
+                    btn.isEnabled = false
+                    println(point_List)
+                    }
             }
         }
     }
-}
+
+
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                main()
+
+            }
+        }
+
+
+
+
+
+
 
 
 
