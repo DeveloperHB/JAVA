c78831953994b8dbc2d1ea1254731a51c6ce74c7
DeveloperHB <167189840+DeveloperHB@users.noreply.github.com>
2024-07-11 13:07:54 +0900

    Add files via upload

--- main/java/org/zerock/domain/BoardVO.java
+++ main/java/org/zerock/domain/BoardVO.java
@@ -1,18 +1,95 @@
 package org.zerock.domain;
 
-import java.sql.Date;
+import java.util.Date;
 
-import lombok.Data;
-
-@Data
 public class BoardVO {
 	
-	private int bno;
-	private String writer;
+	private Integer bno;
 	private String title;
 	private String content;
-	private Date writeDate;
-	private Date updateDate;
+	private String writer;
+	private Date regdate;
+	private int viewcnt;
+	private int replycnt;
+	
 
-}
+	public Integer getBno() {
+		return bno;
+	}
+
+
+	public void setBno(Integer bno) {
+		this.bno = bno;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public String getContent() {
+		return content;
+	}
+
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+
+	public String getWriter() {
+		return writer;
+	}
+
+
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+
+	public int getViewcnt() {
+		return viewcnt;
+	}
+
+
+	public void setViewcnt(int viewcnt) {
+		this.viewcnt = viewcnt;
+	}
+
+
+	public int getReplycnt() {
+		return replycnt;
+	}
+
+
+	public void setReplycnt(int replycnt) {
+		this.replycnt = replycnt;
+	}
+
+
+	@Override
+	public String toString() {
+		return "BoardVO [bno=" + bno + ", title=" + title + ", content=" + content + ", writer=" + writer + ", regdate="
+				+ regdate + ", viewcnt=" + viewcnt + ", replycnt=" + replycnt + "]";
+	}
+	
+
+	}
+
 
